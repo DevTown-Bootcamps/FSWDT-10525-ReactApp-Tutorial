3ed3fc4eb2b5fd56f64bde976264f055bf7ee2a8
[200~anshulojha1214@gmail.com <anshulojha1214@gmail.com>
2025-07-23 17:11:54 +0530

    fetch

--- src/components/Api.js
+++ src/components/Api.js
@@ -1,25 +1,36 @@
+import axios from "axios";
 import React,{useState,useEffect} from "react";
 export default function API(){
   const [product,setProduct]=useState([]);
   useEffect(()=>{
-    fetch("https://flipkart-backend-1-pjtm.onrender.com/products")
-      .then((res)=> res.json())
-      .then((data)=>{
-        let allProducts=[];
-        if(Array.isArray(data)&&data.length>0 && data[0].product){
-            data.forEach((doc)=>{
-               if(Array.isArray(doc.products)){
-                allProducts=allProducts.concat(doc.products);
-               }
-            });
-        }
-        else if(Array.isArray(data)){
-            allProducts=data;
-
-        }
-        setProduct(allProducts);
+    axios.get("https://flipkart-backend-1-pjtm.onrender.com/products")
+      .then(res=> {
+        setProduct(res.data);
+      })
+      .catch(err=>{
+        console.error(err);
       })
   },[]);
+
+//   useEffect(()=>{
+//     fetch("https://flipkart-backend-1-pjtm.onrender.com/products")
+//       .then((res)=> res.json())
+//       .then((data)=>{
+//         let allProducts=[];
+//         if(Array.isArray(data)&&data.length>0 && data[0].product){
+//             data.forEach((doc)=>{
+//                if(Array.isArray(doc.products)){
+//                 allProducts=allProducts.concat(doc.products);
+//                }
+//             });
+//         }
+//         else if(Array.isArray(data)){
+//             allProducts=data;
+
+//         }
+//         setProduct(allProducts);
+//       })
+//   },[]);
 
 
   console.log(product);
